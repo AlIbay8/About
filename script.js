3e24dfa919492b9fb170c79eb632a056c3f1f362
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-12-12 18:01:08 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,5 @@
+$("button").click(function() {
+    var phrase = $("input").val();
+    if (phrase === "2845") {
+    $(".riddle").text("My first is in readable but not in blare. My second is in dinette but not in dent. My third is in somatic but not in atomic. My fourth is in rightly but not in tightly. My fifth is in petulantly but not in aplenty. My sixth is in opinionated but not in indentation. My seventh is in hardhearted but not in redheaded.");}
+});
